acfa53a9589fa20b2ecb2a87de09b11c5797fec0
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-04-23 11:54:33 +0300

    Second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, { Component } from 'react';
+import { GlobalStyle } from "./GlobalStyle";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <GlobalStyle />
+
     </div>
-  );
+    )
+  }
+
 };
+
+export default App;
